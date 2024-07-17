4de594a53446eefbfabf86f3478dbdc98132b7c8
mahmoud radwan <medodiscordban@gmail.com>
2024-07-17 11:43:46 +0300

    Added Viewing Previous Test

--- command_line_test.sh
+++ command_line_test.sh
@@ -4,31 +4,62 @@ echo "Command Line Test"
 currentdate=$(date +"%Y-%m-%d %H:%M:%S")
 cd Data/
 
+makeAnswerFile(){
+    if [  -f AnswerFile.txt ]; then
+    rm -rf AnswerFile.txt
+    touch AnswerFile.txt
+    echo "$currentdate | Created AnswerFile.txt" >> logs.log
+    else
+    touch AnswerFile.txt
+    fi
+}
+
 takeTest(){
     question=1
     lines=("1" "7" "13")
+    alines=("2" "8" "14")
+    blines=("3" "9" "15")
+    clines=("4" "10" "16")
+    dlines=("5" "11" "17")
+    idx=0
+    makeAnswerFile
     while [ $question -le 3 ];do
         timer=10
         randidx=$((RANDOM%${#lines[@]}))
         current=${lines[$randidx]}
         unset lines[$randidx]
         lines=("${lines[@]}")
+        seds=`sed -n "$current, $((current + 5))p" < QuestionBank.txt`
         while [ $timer -gt 0 ];do
             clear
             echo "Time Remaining: ${timer} Seconds"
             echo
             echo -n "$question. "
             sed -n "$current, $((current + 5))p" < QuestionBank.txt
+            echo -n "Please Choose an Option: "
             read -t 1 -n 1 answer && break
+            duration=$((10 - $timer))
             (( timer-- ))
         done
         (( question++ ))
+        echo $seds >> AnswerFile.txt
+        sed -i 's/\s\+\[\+/\n[/g' AnswerFile.txt
+        echo >> AnswerFile.txt
+        if [ $answer == "a" ]; then
+        sed -i "${alines[$idx]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        elif [ $answer == "b" ]; then
+        sed -i "${blines[$idx]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        elif [ $answer == "c" ]; then
+        sed -i "${clines[$idx]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        elif [ $answer == "d" ]; then
+        sed -i "${dlines[$idx]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        fi
+        (( idx++ ))
     done
 }
 
 viewTest(){
-    echo "TODO"
-    #TODO
+    cat AnswerFile.txt
 }
 
 featureScreen(){
--- test.sh
+++ test.sh
@@ -29,15 +29,32 @@ arrayRandom(){
     echo ${!lines[@]}
 }
 
+makeAnswer(){
+    if [ -f AnswerFile.txt ]; then 
+    rm -rf AnswerFile.txt
+    touch AnswerFile.txt
+    else
+    touch AnswerFile.txt
+    fi
+}
+
 randomQuestion(){
     question=1
     lines=( 1 7 13 )
+    alines=(2 8 14)
+    blines=(3 9 15)
+    clines=(4 10 16)
+    dlines=(5 11 17)
+    i=0
+    makeAnswer
     while [ $question -le 3 ]; do
         timer=10
+        sec=$SECONDS
         rand=$((RANDOM%${#lines[@]}))
         current="${lines[$rand]}"
         unset lines[$rand]
         lines=("${lines[@]}")
+        seds=`sed -n "$current, $((current + 5))p" < QuestionBank.txt`
         while [ $timer -gt 0 ]; do
             clear
             echo "Time Remaining: ${timer} Seconds"
@@ -45,10 +62,23 @@ randomQuestion(){
             echo -n "$question. "
             sed -n "$current, $((current + 5))p" < QuestionBank.txt 
             read -t 1 -n 1 answer  && break
-            echo
+            duration=$((10 - $sec))
             (( timer-- ))
         done
         question=$(( question + 1 ))
+        echo $seds >> AnswerFile.txt 
+        sed -i 's/\s\+\[\+/\n[/g' AnswerFile.txt
+        echo >> AnswerFile.txt
+        if [ $answer == "a" ]; then
+        sed -i "${alines[$i]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        elif [ $answer == "b" ]; then
+        sed -i "${blines[$i]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        elif [ $answer == "c" ]; then
+        sed -i "${clines[$i]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        elif [ $answer == "d" ]; then
+        sed -i "${dlines[$i]}s/$/ -> You Answered this Question in $duration Seconds/" AnswerFile.txt
+        fi
+        (( i++ ))
     done
 }
 
